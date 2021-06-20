609767b974573eed077e052ef165627d8900f55f
Rodrigue tech <rodriguet802@gmail.com>
2021-06-20 04:30:59 +0100

    ajout du slug a l'entité plat

--- src/Controller/PlatController.php
+++ src/Controller/PlatController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Plat;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlatController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/plats', name: 'plat')]
     public function index(): Response
     {
+        $plats = $this->entityManager->getRepository(Plat::class)->findAll();
+
         return $this->render('plat/index.html.twig', [
-            'controller_name' => 'PlatController',
+            'plats' => $plats
         ]);
     }
 }
