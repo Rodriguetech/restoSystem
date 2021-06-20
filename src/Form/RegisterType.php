ee99f4ef073e81b3d1b84f40db2d051d832bf099
Rodrigue tech <rodriguet802@gmail.com>
2021-06-20 02:34:34 +0100

    Page inscription

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -3,15 +3,78 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+
+            ->add('nom', TextType::class, [
+                'label' => 'Votre nom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
+                'attr' => [
+                    'class' => 'form-control mt-2'
+                ]
+            ])
+
+            ->add('prenom', TextType::class, [
+                'label' => 'Votre prénom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
+                'attr' => [
+
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 55
+                ]),
+                'attr' => [
+                    'class' => 'form-control mt-2'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'class' => 'form-control mt-2'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'class' => 'form-control mt-2'
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+                'attr' => [
+                    'class' => 'btn btn-primary w-100 btn-block mt-3'
+                ]
+            ])
         ;
     }
 
